2890f58cd992969cddf87e52975ca2e169d14582
S <skanojia@skandha.in>
2023-08-23 12:26:05 +0530

    Initial - Windows Service to generate logs of when machine start and stops , also windows Status made Automatic and Started

---
+++ SnowDrop_WindowsService_MachineLog/SnowDrop/ProjectInstaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnowDrop
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            //To set Service statup type Automatic
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+        }
+
+        //Automatic start after install 
+        protected override void OnAfterInstall(IDictionary savedState)
+        {
+            base.OnAfterInstall(savedState);
+
+            //The following code starts the services after it is installed.
+            using (System.ServiceProcess.ServiceController serviceController = new System.ServiceProcess.ServiceController(serviceInstaller1.ServiceName))
+            {
+                serviceController.Start();
+            }
+        }
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+
+        private void serviceInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
+    }
+}
